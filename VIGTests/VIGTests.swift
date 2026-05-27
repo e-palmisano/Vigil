797adfd2ab506c39ceb582b62d2e0694901a0ea4
e.palmisano <e.palmisano@reply.it>
2026-05-27 12:53:52 +0200

    chore: initial Vigil project setup with xcodegen

---
+++ VIGTests/VIGTests.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+final class VIGTests: XCTestCase {
+    func testPlaceholder() {
+        XCTAssertTrue(true)
+    }
+}
---
+++ Vigil/App/VIGApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main struct VIGApp: App {
+    var body: some Scene {
+        Settings {
+            Text("Vigil")
+                .frame(width: 200, height: 100)
+        }
+    }
+}
